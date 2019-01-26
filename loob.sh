a70af29cfcab0092ab67b033519eb6f717d73f24
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-26 21:45:55 +0200

    push

--- loob.sh
+++ loob.sh
@@ -2,6 +2,6 @@
 echo ""> l.log
 for i in {1..10}
 do
-   curl http://clone-streama.1d35.starter-us-east-1.openshiftapps.com/actuator/info >>l.log
+   curl http://gateway-streama.1d35.starter-us-east-1.openshiftapps.com/clone/actuator/info >>l.log
    echo "" >>l.log
 done
