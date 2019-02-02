0d00e50f486192be86fb95455cc334b343a4cf2b
ashraf <ashraf1abdelrasool@gmail.com>
2019-02-02 14:31:37 +0200

    clone

--- gateway/src/main/java/org/revo/streama/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/revo/streama/gateway/GatewayApplication.java
@@ -1,19 +1,59 @@
 package org.revo.streama.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static reactor.core.publisher.Flux.fromIterable;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RibbonClient(name = "gateway")
 @EnableCircuitBreaker
+@Slf4j
 public class GatewayApplication {
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> function(@Value("${message:default}") String message, DiscoveryClient discoveryClient) {
+        return route(GET("/message"), serverRequest -> ok().body(Mono.just(message), String.class))
+                .andRoute(GET("/services"), serverRequest -> {
+
+                    String object = this.restTemplate.getForObject("http://clone/actuator/info", String.class);
+                    log.info(object);
+                    List<String> services = discoveryClient.getServices();
+                    return ok().body(fromIterable(services.stream().flatMap(it -> discoveryClient.getInstances(it).stream()).collect(Collectors.toList())), ServiceInstance.class);
+                });
     }
 
 }
